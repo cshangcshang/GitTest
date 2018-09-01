bca26b0b2a651de1372e71a07e6b0b0e3c0c5f0a
Administrator <Administrator@PC-20170820XHBV>
2018-09-01 19:47:41 +0800

    用Dev分支 提交代码 2.0

--- GitTest/src/cn/cshang/GitTest.java
+++ GitTest/src/cn/cshang/GitTest.java
@@ -4,5 +4,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("this my first git project");
+		say("hello word");
+	}
+	public static void say(String word) {
+		System.out.println(word);
 	}
 }
