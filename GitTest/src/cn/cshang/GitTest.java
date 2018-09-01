4d23017275226c73e606b068b0110366d2739de1
Administrator <Administrator@PC-20170820XHBV>
2018-09-01 19:58:17 +0800

    我才测试一次试试

--- GitTest/src/cn/cshang/GitTest.java
+++ GitTest/src/cn/cshang/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("this my first git project");
 		say("hello word");
+		System.out.println("我就是还想测试一下");
 	}
 	public static void say(String word) {
 		System.out.println(word);
