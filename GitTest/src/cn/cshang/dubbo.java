e26db0be593b5b4458e20e4c904c5e85f38206cf
zhr <Administrator@USER-20170113YF>
2018-10-22 20:03:26 +0800

    2的N次方 计算失败 周辉荣

---
+++ GitTest/src/cn/cshang/dubbo.java
@@ -0,0 +1,11 @@
+package cn.cshang;
+
+public class dubbo {
+
+	public static void main(String[] args) {
+		System.out.println("分布式开发");
+		long i = 2L;
+		long  j=i<<50L;
+		System.out.println(j);
+	}
+}
