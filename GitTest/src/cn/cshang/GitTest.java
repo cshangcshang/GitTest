07fd3aae2bfbefd578c1a72831804fec34d7e9c3
zhr <Administrator@USER-20170113YF>
2018-10-22 19:34:59 +0800

    ss

--- GitTest/src/cn/cshang/GitTest.java
+++ GitTest/src/cn/cshang/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("this my first git project");
 		say("hello word");
-		System.out.println("我就是还想测试一下,xxx");
+		System.out.println("我就是还想测试一下,xsssxx");
 	}
 	public static void say(String word) {
 		System.out.println(word);
